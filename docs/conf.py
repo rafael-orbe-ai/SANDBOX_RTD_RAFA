d6c618464a44d62a03fb9aa072361a01c5323261
OrbeAI <90585189+OrbeAI@users.noreply.github.com>
2022-01-26 21:20:26 -0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    #'sphinx.ext.intersphinx',
+    'sphinx.ext.intersphinx',
     'sphinx_copybutton'
 ]
 
