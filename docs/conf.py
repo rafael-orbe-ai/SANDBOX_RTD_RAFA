7251d043fe79de9b04bc80f3dcee89c7adc11dfc
GuiHungaro <89612743+GuiHungaro@users.noreply.github.com>
2022-02-10 18:23:31 -0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
-html_logo = 'logo_orbe.png'
+html_logo = 'images/logo/logo_orbe.png'
 
 html_theme_options = {
     'logo_only': True,
